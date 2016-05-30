5d5f13a432ffd67927f64b703c67b90fdbaf5ff9
jianiyang <376692150@163.com>
2016-05-29 20:47:30 -0700

    WeebyChatServer class

--- src/WeebyChatServer.java
+++ src/WeebyChatServer.java
@@ -78,15 +78,239 @@ public class WeebyChatServer extends ChatServer {
     }
 
     protected String getUserName(InputStream in, OutputStream out, Socket sock) throws IOException{
+        byte[] data = new byte[2000];
+        int len = 0;
 
+        String welcome = ARROW + "This is Jiani Yang's Chat Server for Weeby! \n";
+        welcome += ARROW + "Please create a User name. \n";
+        welcome += ARROW;
+
+        try{
+            out.write(welcome.getBytes());
+        }
+        catch (IOException exception){
+            System.err.println("Error printing out the Welcome message!");
+        }
+
+        String userName = "";
+        boolean loop = false;
+
+        while ((len = in.read(data)) != -1){
+            userName = new String(data, 0, len-2);
+            lockSocks.lock();
+            if(!socks.containsKey(userName)){
+                loop = false;
+                socks.put(userName, sock);
+            }
+            else {
+                loop = true;
+                String tryAgain = ARROW + "User Name has been taken. Please pick a new one. \n";
+                tryAgain += ARROW + "New User Name: ";
+
+                try{
+                    out.write(tryAgain.getBytes());
+                }
+                catch (IOException exception){
+                    System.err.println("Error prompting user pick a user name");
+                }
+            }
+            lockSocks.unlock();
+
+            if(!loop){
+                String newUserWelcome = ARROW + "Welcome" + userName + "!\n";
+                try{
+                    out.write(newUserWelcome.getBytes());
+                }
+                catch (IOException exception){
+                    System.err.println("Error Welcoming");
+                }
+                break;
+            }
+        }
+        if(len == -1){
+            sock.close();
+            return "";
+        }
+        return userName;
     }
 
     protected void userCommand(String username, InputStream in, OutputStream out, Socket sock) throws IOException{
+        byte[] data = new byte[2000];
+        int len = 0;
 
+        String commandList = ARROW + "The commands has been listed below.\n";
+        commandList += ARROW + "* /createRoom <Name>: create a chat room.\n";
+        commandList += ARROW + "* /deleteRoom <Name>: delete a chat room.\n";
+        commandList += ARROW + "* /rooms: List all rooms and peope number.\n";
+        commandList += ARROW + "* /join <Name>: Join the particular room.\n";
+        commandList += ARROW + "* /changeUserName <Name>: Change your user name.\n";
+        commandList += ARROW + "* /users: List all the online users.\n";
+        commandList += ARROW + "* /help: List command options.\n";
+        commandList += ARROW + "* /quit: Exit from the chat server.\n";
+
+        try{
+            out.write(commandList.getBytes());
+            out.write(ARROW.getBytes());
+        }
+        catch (IOException exception){
+            System.err.println("Print out commands failed!");
+        }
+
+        while((len = in.read(data)) != -1){
+            String message = new String(data, 0, len);
+            String[] command = message.substring(0, message.length()-2).split(" ");
+            String print = "";
+            if(command[0].equals("/createRoom")){
+                print = createRoom(command);
+            }
+            else if(command[0].equals("/deleteRoom")){
+                print = deleteRoom(command);
+            }
+            else if(command[0].equals("/rooms")){
+                print = printRooms();
+            }
+            else if(command[0].equals("/join")){
+                print = join(command, username, sock);
+                if(print.equals("")) return;
+                else if(print.equals(" ")) print = "";
+            }
+        }
+    }
+
+    private String getRestofCommand(String[] command){
+        StringBuilder sb = new StringBuilder();
+        for(int i = 1; i < command.length; i++){
+            sb.append(command[i]);
+            if(i != command.length - 1){
+                sb.append(" ");
+            }
+        }
+        return sb.toString();
     }
 
     protected void handleClient(Socket sock){
 
+    }
+
+    private String createRoom(String[] command){
+        if(command.length < 2){
+            String incorrectArgs = ARROW + "Please indicate your room name to create.\n";
+            return incorrectArgs;
+        }
+
+        String roomName = getRestofCommand(command);
+        lockChatrooms.lock();
+        chatrooms.put(roomName, new HashSet<String>());
+        lockChatrooms.unlock();
+
+        String created = ARROW + roomName + " created successfully!\n";
+        return created;
+    }
+
+    private String deleteRoom(String[] command){
+        if(command.length < 2){
+            String incorrectArgs = ARROW + "Please indicate your room name to delete.\n";
+            return incorrectArgs;
+        }
+        String roomName = getRestofCommand(command);
+        lockChatrooms.lock();
+        if(!chatrooms.containsKey(roomName)){
+            String noRoom = ARROW + "There's no room called " + roomName + "\n";
+            lockChatrooms.unlock();
+            return noRoom;
+        }
+
+        HashSet<String> members = chatrooms.get(roomName);
+        if(members.size() != 0) {
+            String noDelete = ARROW + "There's someone in it. You are not able to delete it!\n";
+            lockChatrooms.unlock();
+            return noDelete;
+        }
+
+        chatrooms.remove(roomName);
+        lockChatrooms.unlock();
+    }
+
+    private String printRooms(){
+        lockChatrooms.lock();
+        if(chatrooms.size() == 0){
+            String noRooms = ARROW + "There's no room right now.\n";
+            lockChatrooms.unlock();
+            return noRooms;
+        }
+        String rooms = ARROW + "Rooms as following: \n";
+        for(String chatRoomName : chatrooms.keySet()){
+            HashSet<String> members = chatrooms.get(chatRoomName);
+            rooms += ARROW + chatRoomName + " (" + members.size() + ") \n";
+        }
+        lockChatrooms.unlock();
+        return rooms;
+    }
+
+    private String join(String[] command, String username, Socket sock){
+        if(command.length < 2){
+            String incorrectArgs = ARROW + "Please indicate room name.\n";
+            return incorrectArgs;
+        }
+        String roomName = getRestofCommand(command);
+        if(!chatrooms.containsKey(roomName)){
+            String noRoom = ARROW + "That's not an available room.\n";
+            return noRoom;
+        }
+        try {
+            newUserToRoom(username, roomName, sock);
+            if(chat(roomName, username, sock.getInputStream(), sock.getOutputStream(), sock) == -1){
+                return "";
+            }
+        }
+        catch (IOException exception){
+            System.err.println("Error getting " + username + "'s in/out stream");
+        }
+        return " ";
+    }
+
+    private void newUserToRoom(String username, String roomName, Socket newSock){
+        String welcome = ARROW + "Welcome to " + roomName + "!\n" + ARROW;
+        try {
+            newSock.getOutputStream().write(welcome.getBytes());
+        }
+        catch (IOException exception){
+            System.err.println("Error welcoming " + username);
+        }
+
+        lockChatrooms.lock();
+        HashSet<String> members = chatrooms.get(roomName);
+        members.add(username);
+        lockChatrooms.unlock();
+
+        //tell room members, there's new member entered.
+        String message = "Entering room: " + username + "\n";
+        sendMessage(roomName, message, username);
+        StringBuilder users = new StringBuilder();
+        users.append("Current users: \n");
+        Socket s;
+        OutputStream out;
+        lockChatrooms.lock();
+        lockSocks.lock();
+        Iterator<String> it = members.iterator();
+        while (it.hasNext()){
+            String n = it.next();
+            s = socks.get(n);
+            users.append(ARROW).append(" ").append(n).append(" ");
+            if(s == newSock){
+                users.append(YOURSELF);
+            }
+            users.append("\n");
+        }
+        lockSocks.unlock();
+        lockChatrooms.unlock();
+
+        try{
+            newSock.getOutputStream().write(users.toString().getBytes());
+        }
+        catch (IOException exception){
+            System.err.println("Error: message sending failed for: " + username);
+        }
     }
 
     public static void main(String[] arg){
